6369227c58b2b0763da796e6194f3ed7f1abb810
Peter Mashin <petemashine@gmail.com>
2021-04-12 09:20:04 +0300

    solution 4 module

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,10 +1,10 @@
-function camelize(str) {  
+function camelize(str) {
   let str1 = str.split('-') ;
-
-  let strArr = str1.map(function(MassProp) {  
+  let strArr = str1.map(function(MassProp, index) {
+    if (index === 0) return MassProp;
     return MassProp.charAt(0).toUpperCase() + MassProp.slice(1) ;
-}) ;
-
+  });
   return strArr.join('') ;
-}
+  }
+
 
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
 function makeFriendsList(friends) {
-  // ваш код...
-}
+  let example = document.createElement( 'ul' ) ;
+  for (let elem of friends) {
+    let result = `${elem['firstName']} ${elem['lastName']}`
+    example.insertAdjacentHTML( `beforeEnd` ,`<li>${result}</li>`) ;
+    }
+  return example ;
+  }
+
+  
+  
