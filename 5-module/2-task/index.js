49b8c7c9b9c1e564172f0ffd9d659bb1958a8477
Peter Mashin <petemashine@gmail.com>
2021-04-20 08:10:01 +0300

    solution of 5 module

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  const btn = document.querySelector('.toggle-text-button') ;
+  const text = document.querySelector('#text') ;
+
+let textMagic = () => text.classList.toggle('textHidden') ;
+
+btn.addEventListener('click' ,textMagic) ;
+
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector('.carousel__arrow_right') ;
+  const leftButton = document.querySelector('.carousel__arrow_left') ;
+  const picture = document.querySelector('.carousel__inner') ;
+  let step = 0 ;
+  const maxLength = picture.children.length -1 ;
+  leftButton.style.display = 'none' ;
+
+  function nextBtn() {
+    picture.style.transform += `translateX(-${picture.offsetWidth}px)`;
+    step++ ;
+    leftButton.style.display = ''
+    if (step === maxLength ) {
+      rightButton.style.display = 'none'
+        }
+	} 
+
+  function prevBtn() {
+    picture.style.transform += `translateX(${picture.offsetWidth}px)`;
+    step-- ;
+    rightButton.style.display = ''
+    if (step === 0 ) {
+      leftButton.style.display = 'none'
+    }
+}
+
+  rightButton.addEventListener('click', nextBtn) ;
+
+  leftButton.addEventListener('click', prevBtn)
 }
